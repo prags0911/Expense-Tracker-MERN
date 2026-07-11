f21cb4e8ddde5b8323e231e4d0d5b4736ad9c11a
prags0911 <pragyasingh35524@gmail.com>
2026-07-11 21:41:15 +0530

    Update sendMail.js

--- backend/src/utils/sendMail.js
+++ backend/src/utils/sendMail.js
@@ -1,42 +1,10 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// import nodemailer from "nodemailer";
-
-// const transporter = nodemailer.createTransport({
-//   host: "smtp.gmail.com",
-//   port: 587,
-//   secure: false,
-//   auth: {
-//     user: process.env.EMAIL,
-//     pass: process.env.EMAIL_PASSWORD,
-//   },
-// });
-
-// export const sendMail = async (to, subject, text) => {
-//   try {
-//     const info = await transporter.sendMail({
-//       from: process.env.EMAIL,
-//       to,
-//       subject,
-//       text,
-//     });
-
-//     console.log("Mail sent:", info.messageId);
-//   } catch (error) {
-//     console.log("Mail Error:", error);
-//     throw error;
-//   }
-// };
 import nodemailer from "nodemailer";
 
 export const sendMail = async (to, subject, text) => {
   try {
     // Initialize the transporter dynamically when sendMail is called
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // true for 465, false for other ports
+      host: "gmail",
       auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD,
